b054003b798bbbe64d6750cd72b24c13f0ddcc01
Santhiya guna <santhiyaguna04@gmail.com>
2025-09-02 10:16:59 +0530

    Time: 435 ms (30.83%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select p.firstName,p.lastName,a.city,a.state
-from Person p left join Address a
-on p.personId=a.personId;
-
+select p.firstName ,p.lastName,s.city,s.state
+from person p left join 
+Address s on p.personId=s.personId;
